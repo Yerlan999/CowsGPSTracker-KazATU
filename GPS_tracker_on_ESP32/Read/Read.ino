f2bee1a1b540b9692746e3fa84d34fac4be01e3f
Amir Yerlan <fcbayernmunchenerlan999@gmail.com>
2024-02-08 13:24:41 +0600

    problem with getting GPS data back

--- GPS_tracker_on_ESP32/Read/Read.ino
+++ GPS_tracker_on_ESP32/Read/Read.ino
@@ -13,8 +13,7 @@ LiquidCrystal_I2C lcd(0x3F, lcdColumns, lcdRows);
 RF24 radio(4, 5); // "создать" модуль на пинах 9 и 10 Для Уно
 
 byte address[][6] = {"1Node", "2Node", "3Node", "4Node", "5Node", "6Node"}; //возможные номера труб
-
-byte data_send;
+String data_send; 
 
 void setup() {
   lcd.init();
@@ -24,11 +23,13 @@ void setup() {
 
   radio.begin(); //активировать модуль
   radio.setAutoAck(1);         //режим подтверждения приёма, 1 вкл 0 выкл
-  radio.setRetries(0, 15);    //(время между попыткой достучаться, число попыток)
+  radio.setRetries(0,15);    //(время между попыткой достучаться, число попыток)
   radio.enableAckPayload();    //разрешить отсылку данных в ответ на входящий сигнал
   radio.setPayloadSize(32);     //размер пакета, в байтах
 
   radio.openWritingPipe(address[0]);   //мы - труба 0, открываем канал для передачи данных
+  // radio.openReadingPipe(1,address[0]);      //хотим слушать трубу 0
+
   radio.setChannel(0x60);  //выбираем канал (в котором нет шумов!)
 
   radio.setPALevel (RF24_PA_MAX); //уровень мощности передатчика. На выбор RF24_PA_MIN, RF24_PA_LOW, RF24_PA_HIGH, RF24_PA_MAX
@@ -42,27 +43,22 @@ void setup() {
 
 void loop() {
 
-  byte pipeNo, data_got;                          
+  byte pipeNo; String data_got;                          
   while(radio.available(&pipeNo)){    // слушаем эфир со всех труб
     radio.read( &data_got, sizeof(data_got) );         // чиатем входящий сигнал
-
     displayInfo(data_got, data_send);
-    
     Serial.print("Recieved: "); Serial.println(data_got);
   }
   
-
-  Serial.print("Sent: "); Serial.println(data_send);
-
-  displayInfo(data_got, data_send);
-  
-  radio.write(&data_send, sizeof(data_send));
-  data_send++;
-
-  delay(10000);
+  if (Serial.available()){
+    data_send = Serial.readStringUntil('\n');
+    Serial.print("Sent: "); Serial.println(data_send);
+    displayInfo(data_got, data_send);
+    radio.write(&data_send, sizeof(data_send));
+  }
 }
 
-void displayInfo(byte data_got, byte data_send){
+void displayInfo(String data_got, String data_send){
   lcd.clear();
   lcd.setCursor(0, 0);
   lcd.print("Send: " + String(data_send));
--- GPS_tracker_on_ESP32/Write/Write.ino
+++ GPS_tracker_on_ESP32/Write/Write.ino
@@ -5,8 +5,6 @@
 #include "RF24.h"
 #include <LiquidCrystal_I2C.h>
 
-#define BUTTON_PIN 15 // GPIO21 pin connected to button
-
 int lcdColumns = 20;
 int lcdRows = 4;
 
@@ -14,11 +12,10 @@ LiquidCrystal_I2C lcd(0x27, lcdColumns, lcdRows);
 RF24 radio(4, 5); // "создать" модуль на пинах 9 и 10 Для Уно
 
 byte address[][6] = {"1Node","2Node","3Node","4Node","5Node","6Node"};  //возможные номера труб
-byte data_send;
+String data_send;
 int counter = 0;
 
 void setup(){
-  pinMode(BUTTON_PIN, INPUT);
   lcd.init();
   lcd.backlight();
 
@@ -29,7 +26,9 @@ void setup(){
   radio.enableAckPayload();    //разрешить отсылку данных в ответ на входящий сигнал
   radio.setPayloadSize(32);     //размер пакета, в байтах
 
+  // radio.openWritingPipe(address[0]);   //мы - труба 0, открываем канал для передачи данных
   radio.openReadingPipe(1,address[0]);      //хотим слушать трубу 0
+  
   radio.setChannel(0x60);  //выбираем канал (в котором нет шумов!)
 
   radio.setPALevel (RF24_PA_MAX); //уровень мощности передатчика. На выбор RF24_PA_MIN, RF24_PA_LOW, RF24_PA_HIGH, RF24_PA_MAX
@@ -42,26 +41,23 @@ void setup(){
 }
 
 void loop() {
-    byte pipeNo, data_got;                          
+    byte pipeNo; String data_got;                          
     while(radio.available(&pipeNo)){    // слушаем эфир со всех труб
       radio.read( &data_got, sizeof(data_got) );         // чиатем входящий сигнал
-      
+
       displayInfo(data_got, data_send);
-      
       Serial.print("Recieved: "); Serial.println(data_got);
+
+      if (data_got=="GPS"){
+        data_send = "20.45 13.58";
+        radio.write(&data_send, sizeof(data_send));
+        Serial.print("Sent: "); Serial.println(data_send);
+        displayInfo(data_got, data_send);
+      }
    }
-
-  int buttonState = digitalRead(BUTTON_PIN);
-  delay(250);
-  if (buttonState){
-    counter++;
-    displayInfo(data_got, counter);
-  }
-
-
 }
 
-void displayInfo(byte data_got, byte data_send){
+void displayInfo(String data_got, String data_send){
   lcd.clear();
   lcd.setCursor(0, 0);
   lcd.print("Send: " + String(data_send));
