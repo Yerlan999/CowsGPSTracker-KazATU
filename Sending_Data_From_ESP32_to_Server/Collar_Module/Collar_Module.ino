5a49a1a97f5ae68f0a70aedc1ddf18ff9ed692c5
Amir Yerlan <fcbayernmunchenerlan999@gmail.com>
2024-04-21 15:27:02 +0500

    Pop-up with Bar Graphs common function

--- Sending_Data_From_ESP32_to_Server/Collar_Module/Collar_Module.ino
+++ Sending_Data_From_ESP32_to_Server/Collar_Module/Collar_Module.ino
@@ -25,11 +25,11 @@ char latitudeStr[15];
 char longitudeStr[15];
 
 
-bool ENABLE_DEEP_SLEEP = true;
-bool ENABLE_PSM_LORA = true;
-bool ENABLE_PSM_GPS = true;
+bool ENABLE_DEEP_SLEEP = false;
+bool ENABLE_PSM_LORA = false;
+bool ENABLE_PSM_GPS = false;
 
-bool DUMMY_MODE = false;
+bool DUMMY_MODE = true;
 bool CYCLIC = false;
 
 unsigned long lastTime = 0;
